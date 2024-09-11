ef755e139d4e5fb45933198f462427143af308de
Ever-Daniel-Pereyra-Castillo <a.22308051281282@cbtis128.edu.mx>
2024-09-11 15:29:05 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,8 @@
+print("Ejemplo conjuntos set")
+tipodeportes={"Futbol","Tenis","Basquetbol"}
+print(tipodeportes)
+print("Lista elementos del conjunto tipodeportes loop for")
+for pereyra in tipodeportes:
+    print(pereyra)
+
+    
---
+++ mi_diccionario.py
@@ -0,0 +1,12 @@
+print("Ejemplo diccionario")
+datos = {
+  "nombre": "Daniel",
+  "edad": 16,
+  "grado y grupo": "5J"
+}
+print(datos)
+print("Diccionario con loop for")
+for pereyra in datos:
+    print(pereyra)
+
+    
